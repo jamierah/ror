3b6fd164e397f5ba2b17510d7dfe3eff821a457d
jamierah <jamie.d.white@gmail.com>
2014-09-14 18:41:27 +0000

    User Model Added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913024741) do
+ActiveRecord::Schema.define(version: 20140914181814) do
 
   create_table "microposts", force: true do |t|
     t.text     "content"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20140913024741) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
